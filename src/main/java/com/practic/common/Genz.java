8196dc3beaa4cfbae763ad083319acc23daff159
Mdtau <mdtauseef7@outlook.com>
2025-02-20 07:07:30 +0530

    lengthOfLongestSubstring

--- src/main/java/com/practic/common/Genz.java
+++ src/main/java/com/practic/common/Genz.java
@@ -33,9 +33,26 @@ public class Genz {
          //sortReverseOrder(list1);
 
         //System.out.println(isNotAllDistinct(list1));
-        cubeAndGreater(list1);
+       // cubeAndGreater(list1);
+        lengthOfLongestSubstring("asfdsfd");
+    }
 
-
+    public static int lengthOfLongestSubstring(String s) {
+        int[] chars = new int[128];
+        int left = 0, right = 0;
+        int res = 0;
+        while (right < s.length()) {
+            char r = s.charAt(right);
+            chars[r]++;
+            while (chars[r] > 1) {
+                char l = s.charAt(left);
+                chars[l]--;
+                left++;
+            }
+            res = Math.max(res, right - left + 1);
+            right++;
+        }
+        return res;
     }
 
     static void cubeAndGreater(List<Integer> numList){
